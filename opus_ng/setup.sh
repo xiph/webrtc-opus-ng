a8083cc4be2407092e3aea774ea21d1cfa7df1f5
Michael Klingbeil <klingm@amazon.com>
2023-12-13 13:04:25 -0500

    update to latest opus-ng with support for DRED on multiframe packets

--- opus_ng/setup.sh
+++ opus_ng/setup.sh
@@ -6,7 +6,7 @@ cd ./src/opus_ng/opus
 rm -rf src
 git clone -b opus-ng https://gitlab.xiph.org/xiph/opus.git src
 cd ./src
-git reset --hard 72cc88dfddce319aeac075bd28eff791cd2b14d8
+git reset --hard 6d7ae213ce07cd3090a3d77ad0e79035bcf5d191
 ./dnn/download_model.sh df63771
 popd
 rm -rf ./src/third_party/opus
