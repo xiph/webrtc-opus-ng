f5df971bcf0fb56403e6d331e6a3e2b3a476193b
Jim Gustafson <jim@signal.org>
2024-02-27 17:01:48 -0800

    Move all DRED encoding/decoding files to dnn/ dir (#1)

--- opus_ng/setup.sh
+++ opus_ng/setup.sh
@@ -6,7 +6,7 @@ cd ./src/opus_ng/opus
 rm -rf src
 git clone -b main https://gitlab.xiph.org/xiph/opus.git src
 cd ./src
-git reset --hard 57901a6758c3bdc7481d61669812bde13d2085b8
+git reset --hard dcce2fd455b1f407bf4e1347ce5358a6d0096bd2
 ./dnn/download_model.sh "735117b"
 popd
 rm -rf ./src/third_party/opus
